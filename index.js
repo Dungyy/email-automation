b3fa2e125f2b9a8d97ae96d8c83d007239bead6c
Dungyy <erickmunoz13@gmail.com>
2022-04-03 22:20:04 -0500

    Finshed EMAIL Automation

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
-const express = require('express');
+const express = require("express");
+const nodeMailer = require("nodemailer");
+const cron = require("node-cron");
 const app = express();
-const Bree = require('bree');
 const PORT = process.env.PORT || 9000;
 
-require("dotenv").config()
+require("dotenv").config();
 
-const bree = new Bree({
-    jobs : [{
-        name: 'sendEmail',
-        cron: '* * * * *',
-        worker: {
-            workerDate: {
-                description : "This jo wil send emails"
-            }
-        }
-    }]
-})
+// Email Message Options
+const mailOptions = {
+  from: process.env.USER_EMAIL,
+  to: process.env.EMAIL_RECEIVER,
+  subject: "Hey There ❤️",
+  //   text: "Hello  This is Erick Munoz.",
+  html: "<html> <body style='color:#8FBDD3;background-color:#DFDFDE;'> <h2> WELCOME </h2> <br />  <h3>Hey, This is a example of a Email Automation set up</h3><br /> Ok, i gotta go now, back to coding! Dont forget to Check out my Website! </body> <p style='color:#65C18C'>Erick Munoz <br/> Software Developer <a href='https://erickmunoz.tech'> <br/>www.ErickMunoz.Tech</a></p></html>",
+};
 
-bree.start()
+// email Transport config
+const transporter = nodeMailer.createTransport({
+  service: process.env.EMAIL_SERVICE,
+  auth: {
+    user: process.env.USER_EMAIL,
+    pass: process.env.USER_PASS,
+  },
+});
 
-app.listen(PORT, () => console.log(`SERVER RUNNING | ${PORT}`))
+// NODE-CRON Automation Set-up
+// # ┌────────────── second (optional)
+// # │ ┌──────────── minute
+// # │ │ ┌────────── hour
+// # │ │ │ ┌──────── day of month
+// # │ │ │ │ ┌────── month
+// # │ │ │ │ │ ┌──── day of week
+// # │ │ │ │ │ │
+// # │ │ │ │ │ │
+// # * * * * * *
+
+// send email
+cron.schedule("* * * * * * ", () => {
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Email Sent! :) " + "STATUS: " + info.response);
+    }
+  });
+});
+
+// Server
+app.listen(PORT, () => console.log(`SERVER RUNNING | PORT: ${PORT}`));
